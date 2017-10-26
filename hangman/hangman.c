ae30e917c5388ce06b8c6c51af4d78566e7d2bb5
certemail <certemail@hotmail.com>
2017-10-26 15:56:12 -0400

    updated to display board

--- hangman/hangman.c
+++ hangman/hangman.c
@@ -3,10 +3,25 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX_WORDS     100
-#define MAX_WORD_LEN   50
-#define SUCCESS         0
-#define FAILURE         1
+#define MAX_WORDS             100
+#define MAX_WORD_LEN           30 
+#define MAX_NUM_WRONG_GUESSES   5
+
+#define SUCCESS                 0
+#define FAILURE                 1
+
+typedef struct statistics {
+    int wins;
+    int losses;
+    int gameNumber;
+} STATS, *PSTATS;
+
+typedef struct game {
+    int  numCorrectGuess;
+    int  numWrongGuess;
+    char *word;
+    char line[MAX_WORD_LEN];
+} GAME, *PGAME;
 
 int selectWord(char *filepath, char **rWord)
 {
@@ -66,10 +81,43 @@ int selectWord(char *filepath, char **rWord)
     return SUCCESS;
 }
 
+
+void initializeGame(PGAME g, char *rWord)
+{
+    printf("\n%s\n", "initializing game...");
+    int i;
+    char c = '_';
+
+    g->numCorrectGuess = 0;
+    g->numWrongGuess = 0;
+    g->word = rWord;
+
+    memset(g->line, '\0', sizeof(g->line));
+   
+    // fill buffer with underscores based on length of random word
+    for( i = 0; i < strlen(g->word); i++ )
+    {
+        strncat(g->line, &c, sizeof(char));
+    }
+}
+
+void printGame(PGAME g)
+{
+    printf("\n");
+    printf("%s%d\n", "wrong   guesses: ", g->numWrongGuess);
+    printf("%s%d\n", "correct guesses: ", g->numWrongGuess);
+    printf("%s%s\n", "word: ", g->word); 
+    printf("%s%s\n", "line: ", g->line);
+
+    printf("\n"); 
+}
+
 int main(int argc, char *argv[])
 {
+    GAME hangmanGame;
     char *wordlist = "words";
     char *randomWord = NULL;
+    
 
     if (argc > 2) {
         printf("%s%s%s\n", "usage: ", argv[0], " <path_to_word_list>");
@@ -86,6 +134,10 @@ int main(int argc, char *argv[])
     }
     printf("%s%s\n", "random word selected: ", randomWord);
 
+    //initialize game
+    initializeGame(&hangmanGame, randomWord);
+
+    printGame(&hangmanGame);
 
     return 0;
 }
