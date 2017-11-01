c9e3e8e9accc40e4ee26fff37e42a1f759be31c2
certemail <certemail@hotmail.com>
2017-11-01 15:40:48 -0400

    updated

--- word_freq/getopt/getopt.c
+++ word_freq/getopt/getopt.c
@@ -3,21 +3,51 @@
 #include <unistd.h>
 #include <ctype.h>
 
+typedef struct global_args {
+    char *c_num_sorted_words;            // -c <num> as string
+    int num_sorted_words;               // -c <num>
+    int reverse;                        // -r
+    int sort_words_as_numbers;          // -n
+    int sort_words_by_length;           // -l
+    int sort_words_by_scrabble_score;   // -s
+    int sort_words_lexicographically;   // -a
+    int print_only_unique_words;        // -u
+    int print_help_menu;                // -h
+    int num_input_files;                // number of files specified on cmd line
+    char **inputFiles;                  // input files
+} CMD_LINE_ARGS;
+
+
+void display_usage()
+{
+    printf("usage: %s\n", "<prog>");
+    printf("\t%s\n", "-c <number_first_results_to_print>");
+    printf("\t%s\n", "-r print in reverse order");
+    printf("\t%s\n", "-n sort words as if numbers");
+    printf("\t%s\n", "-l sort words by length");
+    printf("\t%s\n", "-s sorts by scrabble score");
+    printf("\t%s\n", "-a sorts lexicographically");
+    printf("\t%s\n", "-u print only unique words");
+    printf("\t%s\n", "-h help menu");
+}
+
 int main(int argc, char **argv)
 {
 
-    int c_flag = 0;
-    int r_flag = 0;
-    int n_flag = 0;
-    int l_flag = 0;
-    int s_flag = 0;
-    int a_flag = 0;
-    int u_flag = 0;
-    int h_flag = 0;
-    
-    char *c_value = NULL;
+    CMD_LINE_ARGS cmd_args;
 
-    int index;
+    cmd_args.c_num_sorted_words = NULL;          // -c <num> as string
+    cmd_args.num_sorted_words = 0;               // -c <num>
+    cmd_args.reverse = 0;                        // -r
+    cmd_args.sort_words_as_numbers = 0;          // -n
+    cmd_args.sort_words_by_length = 0;           // -l
+    cmd_args.sort_words_by_scrabble_score  = 0;  // -s
+    cmd_args.sort_words_lexicographically = 0;   // -a
+    cmd_args.print_only_unique_words = 0;        // -u
+    cmd_args.print_help_menu = 0;                // -h
+    cmd_args.num_input_files = 0;                // number of files specified on cmd line
+    cmd_args.inputFiles = NULL;                  // input files
+
     int c;
 
     opterr = 0;
@@ -27,36 +57,35 @@ int main(int argc, char **argv)
         switch( c )
         {
             case 'c':
-                c_flag = 1;
-                c_value = optarg;
+                cmd_args.c_num_sorted_words = optarg;
                 break;
 
             case 'r':
-                r_flag = 1;
+                cmd_args.reverse = 1;
                 break;
 
            case 'n':
-                n_flag = 1;
+                cmd_args.sort_words_as_numbers = 1;
                 break;
 
             case 'l':
-                l_flag = 1;
+                cmd_args.sort_words_by_length = 1;
                 break;
 
             case 's':
-                s_flag = 1;
+                cmd_args.sort_words_by_scrabble_score = 1;
                 break;
 
             case 'a':
-                a_flag = 1;
+                cmd_args.sort_words_lexicographically = 1;
                 break;
 
             case 'u':
-                u_flag = 1;
+                cmd_args.print_only_unique_words = 1;
                 break;
 
             case 'h':
-                h_flag = 1;
+                cmd_args.print_help_menu = 1;
                 break;
 
             case '?':
@@ -92,19 +121,18 @@ int main(int argc, char **argv)
      printf( "%s%d\n", "h_flag: ", h_flag );
 #endif
 
-    if ( h_flag ) {
-        printf("usage: %s\n", argv[0]);
-        printf("\t%s\n", "-c <number_first_results_to_print>");
-        printf("\t%s\n", "-r print in reverse order");
-        printf("\t%s\n", "-n sort words as if numbers");
-        printf("\t%s\n", "-l sort words by length");
-        printf("\t%s\n", "-s sorts by scrabble score");
-        printf("\t%s\n", "-a sorts lexicographically");
-        printf("\t%s\n", "-u print only unique words");
-        printf("\t%s\n", "-h help menu");
+    // [ -h ] display help menu
+    if ( cmd_args.print_help_menu ) {
+        display_usage();
     }
 
-
+    // [ -c ] print only first n results of sorted list
+    else if ( cmd_args.c_num_sorted_words )
+    {
+        printf("cmd_args.c_value: %s\n", cmd_args.c_num_sorted_words );
+        cmd_args.num_sorted_words = atoi(cmd_args.c_num_sorted_words);
+        printf("cmd_args.num_sorted_words: %d\n", cmd_args.num_sorted_words );
+    }
         
     return 0;
 }
