b9a5c82f03caad2e67a4abd1c2eaa8b513ae34d6
rob <certemail@hotmail.com>
2017-11-30 06:17:12 -0500

    updated

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+def swap(array, first_index, second_index):
+    temp = array[first_index]
+    array[first_index] = array[second_index]
+    array[second_index] = temp
+
 def index_of_minimum(array, start_index):
     print("getting index of minimum, starting at index: " + str(start_index))
     min_value = array[start_index];
@@ -14,10 +19,18 @@ def index_of_minimum(array, start_index):
             min_value = array[min_index]
     return min_index;
 
+def selection_sort(array):
+    for i in range(len(array)):
+        idx_min = index_of_minimum(array, i)
+        swap(array, i, idx_min)
+
 array = [18, 6, 66, 44, 9, 22, 14];
 print("ARRAY:" + str(array))
+print("-----unsorted--------")
 
-index = index_of_minimum(array, 2);
+selection_sort(array)
+print("-----sorted--------")
+print("ARRAY:" + str(array))
 
-print("the index of the minimum value of the subarray starting at index 2 is: " + str(index))
+
 
