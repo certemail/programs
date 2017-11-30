9bcef7fc9fd63853e8a9163713d7014114b08cfa
certemail <certemail@hotmail.com>
2017-11-30 15:33:12 -0500

    updated

--- fibonacci/asm/fibonacci.s
+++ fibonacci/asm/fibonacci.s
@@ -1,65 +1,49 @@
 .intel_syntax noprefix	
 
 .fmtstr:
-    .asciz "Computing fib(%d)...\n"
+    .asciz "Result: 0x%016x\n"
 
 .globl main
 main:
 
         # get argv[1]
-        #xor rbx, rbx
-        #xor rax, rax
+        mov rbx, rsi                # rbx = argv
+        add rbx, 8
+        mov rbx, [rbx]
 
-        #mov rbx, rsi
-        #mov rbx, QWORD PTR [rbx]
-        #add rbx, 0x8
-        #mov rax, QWORD PTR [rbx]
-        
-        #------------------
-        
-        mov rcx, 0x7    # TODO -> rcx will contain user input [ fib(n) ]
-        add rcx, 0x1    # add 1 so loop from <= n
+        # convert to int
+        mov rdi, rbx
+        call atoi                   
+    
+        mov rcx, rax                # rcx = rax (result from atoi)
 
-        mov rdx, 0x3    # rdx starts at 3
+        add rcx, 0x1                # add 1 so loop from <= n
+
+        mov rdx, 0x3                # rdx starts at 3
         
         # compute fibonacci
-
-        mov r8,  0x1    # f_0 = 1
-        mov r9,  0x1    # f_1 = 1
+        mov r8,  0x1                # f_0 = 1
+        mov r9,  0x1                # f_1 = 1
 
 L1:
         cmp rdx, rcx
         je L2
         
         add r8, r9
-        mov rax, r8     # rax = r8 + r9  (result = f_0 + f_1)
+        mov rax, r8                 # rax = r8 + r9 (result = f_0 + f_1)
 
-        mov r8, r9      # r8  = r9      (f_0 = f_1)
-        mov r9, rax     # r9  = result  (f_1 = result)
+        mov r8, r9                  # r8  = r9      (f_0 = f_1)
+        mov r9, rax                 # r9  = result  (f_1 = result)
         
         add rdx, 1
         jmp L1
 L2:
-         
-        ret
+        # print result 
+        mov rdi, OFFSET .fmtstr     # rdi = arg1 -> format string
+        mov rsi, rax                # rsi = arg2 -> result (rax)
 
+        xor rax, rax                # zero rax (no floating points passed)
 
-
-#main:
-#    mov ecx, 99
-#
-#1:
-#    mov rdi, OFFSET .bottles
-#    mov esi, ecx
-#    xor rax, rax
-#    push rcx
-#
-#    call printf
-#
-#    pop rcx
-#    loop 1b
-#
-#    ret
-#
-#.bottles:
-#    .asciz "%d bottles of beer on the wall\n"
+        call printf
+                
+        ret 
