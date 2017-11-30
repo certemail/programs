a16ef1adc14215cb90d3f3d944e5aa5dd0f365a7
certemail <certemail@hotmail.com>
2017-11-30 16:00:05 -0500

    updated

--- fibonacci/asm/fibonacci.s
+++ fibonacci/asm/fibonacci.s
@@ -5,29 +5,36 @@
 
 .globl main
 main:
-
-        # get argv[1]
+        # -- get argv[1]
         mov rbx, rsi                # rbx = argv
-        add rbx, 8
-        mov rbx, [rbx]
+        add rbx, 8                  # rbx = &(argv[1])
+        mov rbx, [rbx]              # rbx = argv[1]
 
-        # convert to int
+        # -- convert to int
         mov rdi, rbx
         call atoi                   
-    
+
+        # -- handle base cases 
+        cmp rax, 0x0                # fib(0) = 0
+        je PRINT
+
+        cmp rax, 0x1                # fib(1) = 1
+        je PRINT
+
+        cmp rax, 0x2                # fib(2) = 1
+        je CASE2                    # jump to special case to set rax = 1
+
         mov rcx, rax                # rcx = rax (result from atoi)
-
         add rcx, 0x1                # add 1 so loop from <= n
-
-        mov rdx, 0x3                # rdx starts at 3
+        mov rdx, 0x3                # start fib(n) calcuation at n=3
         
-        # compute fibonacci
+        # -- compute fib(n)
         mov r8,  0x1                # f_0 = 1
         mov r9,  0x1                # f_1 = 1
 
-L1:
-        cmp rdx, rcx
-        je L2
+LOOP_FIB:
+        cmp rdx, rcx                # rcx contains n
+        je PRINT
         
         add r8, r9
         mov rax, r8                 # rax = r8 + r9 (result = f_0 + f_1)
@@ -36,9 +43,13 @@ L1:
         mov r9, rax                 # r9  = result  (f_1 = result)
         
         add rdx, 1
-        jmp L1
-L2:
-        # print result 
+        jmp LOOP_FIB
+
+CASE2:
+        mov rax, 0x1                # rax = 1 ( for fib(2) )
+
+PRINT:
+        # -- print result 
         mov rdi, OFFSET .fmtstr     # rdi = arg1 -> format string
         mov rsi, rax                # rsi = arg2 -> result (rax)
 
---
+++ fibonacci/asm/test_fib.sh
@@ -0,0 +1,15 @@
+./fibonacci 0
+./fibonacci 1
+./fibonacci 2
+./fibonacci 3
+./fibonacci 4
+./fibonacci 5
+./fibonacci 6
+./fibonacci 7
+./fibonacci 8
+./fibonacci 9
+./fibonacci 10
+
+echo
+echo "testing fib(47):"
+./fibonacci 47
