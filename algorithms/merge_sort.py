d273886d8bfd52832c8a1ecb51adb7399b897ef2
rob <certemail@hotmail.com>
2017-12-04 05:23:49 -0500

    updated

--- algorithms/merge_sort.py
+++ algorithms/merge_sort.py
@@ -1,70 +1,60 @@
 #!/usr/bin/env python
 
+import sys
 import math
 
 SENTINEL = math.inf
 
 def merge(A, p, q, r):
-    print("[merge] p, q, r: " + str(p) + ", " + str(q) + ", " + str(r))
+    sys.stdout.write("[merge]: entering merge()...\n")
+    sys.stdout.flush()
+    sys.stdout.write("[merge]: p, q, r --> " + str(p) + ", " + str(q) + ", " + str(r) + "\n")
+    sys.stdout.flush()
 
-    left_li = []
-    left_li.extend( A[p : q+1] )
+    low_half = []
+    low_half.extend( A[p : q+1] )
 
-    right_li = []
-    right_li.extend(A[q+1 : r +1 ] )
+    high_half = []
+    high_half.extend(A[q+1 : r+1 ] )
 
-    left_li.append(SENTINEL)
-    right_li.append(SENTINEL)
+    sys.stdout.write("[merge]: low_half  --> " + str(low_half) + "\n")
+    sys.stdout.flush()
+    sys.stdout.write("[merge]: high_half --> " + str(high_half) + "\n")
+    sys.stdout.flush()
+
+    # add sentinel value (infinity)
+    low_half.append(SENTINEL)
+    high_half.append(SENTINEL)
 
     i = 0
     j = 0
 
     for k in range(p, r+1):
-        if left_li[i] <= right_li[j]:
-            A[k] = left_li[i]
+        if low_half[i] <= high_half[j]:
+            A[k] = low_half[i]
             i += 1
         else:
-            A[k] = right_li[j]
+            A[k] = high_half[j]
             j += 1
 
-#    k = p
-#    i = 0
-#    j = 0
-#
-#    low_half  = A[:q]
-#    high_half = A[q:]
-#    print("[merge] low_half: "+ str(low_half))
-#    print("[merge] high_half "+ str(high_half))
-#
-#    while  i < len(low_half) and j < len(high_half):
-#        if low_half[i] < high_half[j]:
-#            A[k] = low_half[i]
-#            i += 1
-#        else:
-#            A[k] = high_half[j]
-#            j +=1
-#
-#        k += 1
+    sys.stdout.write("[merge]: complete -->  " + str(A) + "\n")
+    sys.stdout.flush()
 
-    # either low_half or high_half has been fully copied into A
-#    while i < len(low_half):
-#        A[k] = low_half[i]
-#        i += 1
-#        k += 1
-#
-#    while j < len(high_half):
-#        A[k] = high_half[j]
-#        j += 1
-#        k += 1
 
 def merge_sort(array, p, r):
-    print("[merge_sort] arr: " + str(array))
-    print("[merge_sort] p: " + str(p))
-    print("[merge_sort] r: " + str(r))
+    sys.stdout.write("[merge_sort]: entering merge_sort()...\n")
+    sys.stdout.flush()
+    sys.stdout.write("[merge_sort]: arr --> " + str(array) + "\n")
+    sys.stdout.flush()
+    sys.stdout.write("[merge_sort]: p --> " + str(p) + "\n")
+    sys.stdout.flush()
+    sys.stdout.write("[merge_sort]: r --> " + str(r) + "\n")
+    sys.stdout.flush()
 
     if ( p < r):
         q = math.floor( ( p + r ) / 2 )
-        print("[merge_sort] q: " + str(q))
+        sys.stdout.write("[merge_sort]: q --> " + str(q) + "\n")
+        sys.stdout.flush()
 
         merge_sort( array, p, q )   # left subarray
         merge_sort( array, q+1, r ) # right subarray
@@ -73,8 +63,10 @@ def merge_sort(array, p, r):
 
 arr = [3, 7, 12, 14, 2, 6, 9, 11]
 print(arr)
+print("---------------------")
 
 merge_sort( arr, 0, len(arr)-1 )
+print("---------------------")
 print(arr)
 
 #p = 0
