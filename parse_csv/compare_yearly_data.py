93e7051e4f414f6cf77bbae10fbb042185df58d2
rob <certemail@hotmail.com>
2018-03-08 05:24:44 -0500

    updated to write to disk

--- parse_csv/compare_yearly_data.py
+++ parse_csv/compare_yearly_data.py
@@ -1,9 +1,28 @@
-import csv, os, argparse
+import csv, os, argparse, platform
 
 COL_A = 0
 COL_B = 1
 
+def write_to_file(uids):
+    """writes solution to disk"""
+    output_file = "output" + ".csv" 
+    print("\nWriting result to: ", output_file)
+
+    if platform.system() == 'Windows':
+        with open(output_file, 'w', newline='') as csvfile:
+            wtr = csv.writer(csvfile, dialect='excel')
+            for uid in uids:
+                wtr.writerow([uid])
+
+    # running on non-Windows
+    else:
+        with open(output_file, 'w') as csvfile:
+            wtr = csv.writer(csvfile, dialect='excel')
+            for uid in uids:
+                wtr.writerow([uid])
+
 def compare_student_data(input_file):
+    """returns a sorted list of items found in A but NOT in B"""
     print("Parsing", str(input_file), "...")
 
     column_a = []
@@ -38,8 +57,8 @@ def compare_student_data(input_file):
     print("UIDs in column A but NOT in column B: ")
 
     uids_in_col_a_sorted = sorted(list(uids_in_a_but_not_in_b))
-    for i, uid in enumerate(uids_in_a_but_not_in_b):
-        print(i+1,")",uid)
+
+    return uids_in_col_a_sorted
 
 def main():
     parser = argparse.ArgumentParser(description="compare student data")
@@ -51,7 +70,13 @@ def main():
         print(infile + " does not exist. Exiting...")
         exit(1)
     
-    compare_student_data(infile)
+    # get uids only found in column A but NOT in column B
+    uids_unique_to_col_a = compare_student_data(infile)
+    for i, uid in enumerate(uids_unique_to_col_a):
+        print(i+1,")",uid)
+
+    # write out result to disk
+    write_to_file(uids_unique_to_col_a)
 
 if __name__ == '__main__':
     main()
