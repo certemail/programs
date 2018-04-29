597b4f38450db8c571989005230954472aba9b8d
rob <certemail@hotmail.com>
2018-04-29 06:21:47 -0400

    updated

--- bins_and_balls_simulator/bins_and_balls_simulator_expected_tosses_k_balls_in_bin.py
+++ bins_and_balls_simulator/bins_and_balls_simulator_expected_tosses_k_balls_in_bin.py
@@ -3,12 +3,11 @@ import argparse
 import random
 import logging
 import secrets 
-
+import sys
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("num_bins", help="number of bins")
-    parser.add_argument("num_balls", help="number of balls")
     parser.add_argument("num_trials", help="number of trials")
     parser.add_argument("k_balls", help="number of bins with exactly k balls")
     parser.add_argument("--log", help="log level")
@@ -18,58 +17,54 @@ def main():
         numeric_level = getattr(logging, args.log.upper(), None)
         if not isinstance(numeric_level, int):
             raise ValueError('Invalid log level: %s' % loglevel)
-        logging.basicConfig(filename='log.txt', filemode='w', level=numeric_level, format='%(levelname)s: %(message)s')
+        logging.basicConfig(filename='log.txt', \
+                            filemode='w', \
+                            level=numeric_level, \
+                            format='%(levelname)s: %(message)s')
+                            #stream=sys.stdout, level=numeric_level, format='%(levelname)s: %(message)s')
 
     NUM_BINS = int(args.num_bins)
-    NUM_BALLS = int(args.num_balls) 
     K_BALLS = int(args.k_balls)
     NUM_TRIALS = int(args.num_trials)
     
-    total_tosses = 0
-    total_bins_empty = 0
+    print("number bins:   {}".format(NUM_BINS))
+    print("k balls:       {}".format(K_BALLS))
+    print("running {:,} trials ...".format(NUM_TRIALS))
 
+    total_tosses = 0
 
     for i in range(NUM_TRIALS):
-        #print("\n****TRIAL {} ****".format(i+1))
+        logging.debug("\n**** TRIAL {} ****".format(i+1))
 
         # initialize all bins to have zero balls
         bins = [0 for i in range(NUM_BINS)]
 
-        number_of_tosses_until_k_balls_in_bin = 0
+        tosses = 0
 
         # begin tossing...
-        for i in range(NUM_BALLS):
+        while True:
+
+            # ball is up in the air...
+            tosses += 1
 
             # randomly select bin
             rng = secrets.SystemRandom()
             random_bin = rng.randint(0, NUM_BINS-1)
-            #print("toss #{} --> goes into bin#{}".format( (i+1), (random_bin+1)))
+            logging.debug("toss #{} --> goes into bin {}".format( (tosses), (random_bin+1)))
 
             # add ball to random bin
             bins[random_bin] += 1
 
-            #print("\tbins: ", str(bins))
+            logging.debug("\t  bins: {}".format(bins))
 
-            # check if any bin now has K balls
             if K_BALLS in bins:
-                number_of_tosses_until_k_balls_in_bin = i+1
-                total_tosses += i+1
+                number_of_tosses_until_k_balls_in_bin = tosses 
+                total_tosses += tosses 
                 break
 
-            #print()
-
-        total_bins_empty += bins.count(0)
-        #print("-----------------------------")
-        #print("number of empty bins: {}".format(bins.count(0)))
-        #print("number of tosses until one of the bins has {} balls: {}".format(K_BALLS, number_of_tosses_until_k_balls_in_bin))
-    
-    print("\n**************************************")
-    print("total empty bins: {}".format(total_bins_empty))
-    print("average number of empty bins: {:.4f}".format( float(total_bins_empty) / float(NUM_TRIALS)))
-    print()
+    print("----------------------------")
     print("total tosses until a bin has {} balls: {}".format(K_BALLS, total_tosses))
     print("average number of tosses until a bin has {} balls: {:.4f}".format( K_BALLS, (float(total_tosses) / float(NUM_TRIALS ))))
-    print("**************************************")
     
 if __name__ == '__main__':
     main()
